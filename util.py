b2bebef5f11a747c9a048a7c87ca1954c708d0c4
Ivan Kozik <ivan@ludios.org>
2019-01-05 03:13:30 +0000

    Port to Python 3

--- util.py
+++ util.py
@@ -1,33 +1,33 @@
 import subprocess
 
 def get_site(origin_url):
-	if origin_url.startswith(b"https://github.com/"):
+	if origin_url.startswith("https://github.com/"):
 		return "github.com"
-	elif origin_url.startswith(b"git@github.com:"):
+	elif origin_url.startswith("git@github.com:"):
 		return "github.com"
-	elif origin_url.startswith(b"git://kernel.ubuntu.com/"):
+	elif origin_url.startswith("git://kernel.ubuntu.com/"):
 		return "kernel.ubuntu.com"
-	elif origin_url.startswith(b"https://anonscm.debian.org/git/"):
+	elif origin_url.startswith("https://anonscm.debian.org/git/"):
 		return "anonscm.debian.org"
-	elif origin_url.startswith(b"https://chromium.googlesource.com/"):
+	elif origin_url.startswith("https://chromium.googlesource.com/"):
 		return "chromium.googlesource.com"
 
 def get_github_user_and_repo(origin_url):
-	if origin_url.startswith(b"https://github.com/"):
-		_, _, _, user, repo = origin_url.split(b"/")
-	elif origin_url.startswith(b"git@github.com:"):
-		_, user_repo = origin_url.split(b":", 1)
-		user, repo   = user_repo.split(b"/", 1)
+	if origin_url.startswith("https://github.com/"):
+		_, _, _, user, repo = origin_url.split("/")
+	elif origin_url.startswith("git@github.com:"):
+		_, user_repo = origin_url.split(":", 1)
+		user, repo   = user_repo.split("/", 1)
 	else:
 		raise ValueError("remote.origin.url did not start with https://github.com/ or git@github.com:")
-	if repo.endswith(b".git"):
+	if repo.endswith(".git"):
 		repo = repo[:-4]
 	return user, repo
 
-assert get_github_user_and_repo(b"https://github.com/user/repo")     == (b"user", b"repo")
-assert get_github_user_and_repo(b"https://github.com/user/repo.git") == (b"user", b"repo")
-assert get_github_user_and_repo(b"git@github.com:user/repo")         == (b"user", b"repo")
-assert get_github_user_and_repo(b"git@github.com:user/repo.git")     == (b"user", b"repo")
+assert get_github_user_and_repo("https://github.com/user/repo")     == ("user", "repo")
+assert get_github_user_and_repo("https://github.com/user/repo.git") == ("user", "repo")
+assert get_github_user_and_repo("git@github.com:user/repo")         == ("user", "repo")
+assert get_github_user_and_repo("git@github.com:user/repo.git")     == ("user", "repo")
 
 def get_origin_url():
 	"""
@@ -35,4 +35,4 @@ def get_origin_url():
 	"""
 	# Why not git ls-remote?  Because it won't work in a detached HEAD state.
 	# See https://goo.gl/yc3QOe
-	return subprocess.check_output([b"git", b"config", b"--get", b"remote.origin.url"]).rstrip()
+	return subprocess.check_output(["git", "config", "--get", "remote.origin.url"]).decode("utf-8").rstrip()
